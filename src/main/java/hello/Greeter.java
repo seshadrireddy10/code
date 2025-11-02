69e1bfc8edc9bd21ad9b7a2320e518d75bce2b4e
seshadrireddy10 <seshadrireddy@1492gmail.com>
2025-11-02 15:42:22 +0530

    i have changed the Greeter file

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for prasad Technologies";
+        return "DevOps Project for devops Technologies";
     }
 }
